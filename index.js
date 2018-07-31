329b2270ddbde3abd01401807d4b70ae3568a5ba
Markin Igor <markin.io210@gmail.com>
2018-07-31 12:54:29 +0300

    Set up writing games results to DB.

--- index.js
+++ index.js
@@ -6,42 +6,102 @@ const { Client } = require('pg');
 dotenv.load();
 
 const run = async () => {
-  // const client = new Client();
-  // await client.connect();
-  //
-  // const res = await client.query('SELECT $1::text as message', ['Hello world!']);
-  // console.log(res.rows[0].message); // Hello world!
-  // await client.end();
+  try {
+    const dbClient = await postgresConnect();
+    const wsClient = initializeWebSocketClient();
+    const gamesDataPool = [];
+    let recordsWritten = 0;
 
+    wsClient.onopen = () => {
+      console.log('WebSocket Client Connected');
+      wsClient.send(JSON.stringify({ event: "pusher:subscribe", data: { channel: "dice_BTC" } }));
+    };
+
+    wsClient.onmessage = (e) => {
+      if (typeof e.data === 'string') {
+        const message = e.data;
+        const messageData = JSON.parse(JSON.parse(message).data);
+        'win' in messageData &&
+        gamesDataPool.push(messageData);
+      }
+    };
+
+    wsClient.onclose = async () => {
+      console.log('WebSocket Client Closed');
+      await postgresDisconnect(dbClient);
+    };
+
+    const writeInterval = setInterval(async () => {
+      const toWrite = gamesDataPool.splice(0, gamesDataPool.length);
+      console.log(`Writing ${toWrite.length} records`);
+      await writeGamesResult(dbClient, toWrite);
+      recordsWritten += toWrite.length;
+    }, 1000);
+
+    setTimeout(() => {
+      console.log(`Written ${recordsWritten} records.`);
+      clearInterval(writeInterval);
+      wsClient.close();
+    }, 10000);
+
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+
+};
+
+const writeGamesResult = async (client, gamesResults) => {
+  const formattedResults = gamesResults.map(formatGameResult);
+  const valuesQuery = formattedResults.reduce((string, item, i, self) => {
+    string += `(${item.join(',')})${ i < self.length - 1 ? ',' : ''}`;
+    return string;
+  }, '');
+
+  const query =  `INSERT INTO games(win, did, time, nick, imgid, cur, bet, tar, roll, pr) VALUES ${valuesQuery}`;
+  await client.query(query);
+};
+
+const initializeWebSocketClient = () => {
   const client = new W3CWebSocket(process.env.WS_SERVER, 'echo-protocol');
 
   client.onerror = function() {
     console.log('Connection Error');
   };
 
-  client.onopen = function() {
-    console.log('WebSocket Client Connected');
-
-    // function sendNumber() {
-    //   if (client.readyState === client.OPEN) {
-    //     var number = Math.round(Math.random() * 0xFFFFFF);
-    //     client.send(number.toString());
-    //     setTimeout(sendNumber, 1000);
-    //   }
-    // }
-    // sendNumber();
-  };
-
-  client.onclose = function() {
-    console.log('echo-protocol Client Closed');
-  };
-
-  client.onmessage = function(e) {
-    if (typeof e.data === 'string') {
-      console.log("Received: '" + e.data + "'");
-    }
-  };
+  return client;
 };
 
+const postgresConnect = async () => {
+  const client = new Client();
+  await client.connect();
+  console.log('Database connected');
+  return client;
+};
+
+const postgresDisconnect = async (client) => {
+  console.log('Database connection closed');
+  await client.end();
+};
+
+
+const formatGameResult = (data) => {
+  const { win, did, time, nick, imgid, cur, bet, tar, roll, pr } = data;
+
+  const formattedData = {
+    win: !!win,
+    did,
+    time: `to_timestamp(${time})`,
+    nick: `'${nick}'`,
+    imgid,
+    cur: `'${cur}'`,
+    bet: Number(bet),
+    tar: tar === '>52',
+    roll: Number(roll),
+    pr: Number(pr)
+  };
+
+  return Object.values(formattedData);
+};
 
 run().then(() => {});
--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE games (
     win boolean, -- result of the game
-    did integer NOT NULL, -- identifier
-    time date, -- game time
+    did bigint, -- identifier
+    time timestamp, -- game time
     nick varchar(30), -- user nickname
-    imgid integer NOT NULL, -- ???
+    imgid integer, -- ???
     cur varchar(3), -- currency
     bet double precision, -- bet amount
     tar boolean, -- target: <48 (true) or >52 (false)
